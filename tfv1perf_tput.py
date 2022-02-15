b00f0d6a32626c2c8fc4a014900279c4b477c2a2
Alwaysproblem <reganyang0415@gmail.com>
2022-02-15 14:00:17 +0800

    fix space

--- tfv1perf_tput.py
+++ tfv1perf_tput.py
@@ -1,21 +1,44 @@
+#!/usr/bin/env python3
 from tfv1perf import Evaluator
 import pandas as pd
 import argparse
 
 pd.set_option("display.max_columns", 20)
 
+
 class TFv1Evaluator(Evaluator):
-    pass
+  pass
+
 
 if __name__ == "__main__":
 
   parser = argparse.ArgumentParser("the kvparser data extraction")
-  parser.add_argument("-m", "--model", dest="model_path", required=True, type=str)
-  parser.add_argument("-bs", "--batch-size", dest="batch_size", type=int, default=1)
-  parser.add_argument("-ipus", "--num-of-ipus", dest="num_ipus", type=int, default=1)
-  parser.add_argument("-ps", "--pipeline-stages", dest="pipeline_stages", type=int, default=0)
+  parser.add_argument("-m",
+                      "--model",
+                      dest="model_path",
+                      required=True,
+                      type=str)
+  parser.add_argument("-bs",
+                      "--batch-size",
+                      dest="batch_size",
+                      type=int,
+                      default=1)
+  parser.add_argument("-ipus",
+                      "--num-of-ipus",
+                      dest="num_ipus",
+                      type=int,
+                      default=1)
+  parser.add_argument("-ps",
+                      "--pipeline-stages",
+                      dest="pipeline_stages",
+                      type=int,
+                      default=0)
   parser.add_argument("-ths", "--threads", dest="threads", type=int, default=1)
-  parser.add_argument("-i", "--iterations", dest="iterations", type=int, default=100)
+  parser.add_argument("-i",
+                      "--iterations",
+                      dest="iterations",
+                      type=int,
+                      default=100)
   args = parser.parse_args()
 
   if args.pipeline_stages is not None:
